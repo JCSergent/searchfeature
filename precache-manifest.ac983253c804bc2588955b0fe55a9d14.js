4a782dfe572fa5e1f89e2d855e0872ff4f7354b1
JC Sergent <jcsergent20@gmail.com>
2020-09-28 21:26:34 -0400

    Updates

---
+++ precache-manifest.ac983253c804bc2588955b0fe55a9d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c5f27227111fc0eab47",
+    "url": "/searchfeature/static/css/main.099dc919.chunk.css"
+  },
+  {
+    "revision": "2c5f27227111fc0eab47",
+    "url": "/searchfeature/static/js/main.41bcd1c6.chunk.js"
+  },
+  {
+    "revision": "21b1faece464d002f4a0",
+    "url": "/searchfeature/static/js/runtime~main.02d3d6c9.js"
+  },
+  {
+    "revision": "d159d357378f7aff4c8f",
+    "url": "/searchfeature/static/js/2.f110dc6e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/searchfeature/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "00d0ff7f3c707f4ed1a41241af6b854a",
+    "url": "/searchfeature/index.html"
+  }
+];
